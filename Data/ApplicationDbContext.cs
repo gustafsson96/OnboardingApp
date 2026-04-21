ed45c08ba2c8eb122ac0e77546e4985e33d6c93b
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-04-21 13:51:27 -0400

    Update ApplicationDbContext to include ApplicationUser and display first name to dashboard

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using OnboardingApp.Models;
 
 namespace OnboardingApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) { }
